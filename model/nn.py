5748ad10b54c3c0f63049a83cd6b349bb711f3b5
James Jiang <jamesjiang52@gmail.com>
2021-10-06 21:50:11 -0400

    Updates to model

--- model/nn.py
+++ model/nn.py
@@ -1,148 +1,171 @@
 import numpy as np
 import tensorflow as tf
-from keras.models import Model
-from keras.layers import Input, Dense, Conv2D, Flatten, BatchNormalization, Activation, ReLU, Add
-from keras import regularizers
-from keras import callbacks
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Conv2D, Flatten, BatchNormalization, ReLU, add
+from tensorflow.keras import optimizers
+from tensorflow.keras import regularizers
 
 import rules
+from board import Board
 import nn_cfg
 
 
-class NeuralNetwork():
+class NeuralNetwork(Model):
     def __init__(self):
-        self.inputs = Input(
-            shape=(nn_cfg.NUM_INPUT_PLANES, rules.BOARD_SIDE_LENGTH, rules.BOARD_SIDE_LENGTH),
-            name="inputs"
-        )
+        super(NeuralNetwork, self).__init__()
 
-        layers = self._conv_block(self.inputs)
+        self.layer_list = []
+        self.policy_head_layers = []
+        self.value_head_layers = []
+
+        self._conv_block_init()
         for i in range(nn_cfg.NUM_RES_BLOCKS):
-            layers = self._residual_block(layers)
-
-        policy_out = self._policy_head(layers)
-        value_out = self._value_head(layers)
-
-        self.model = Model(
-            inputs=[self.inputs],
-            outputs=[policy_out, value_out]
-        )
-
-        self.model.compile(
-            optimizer=tf.keras.optimizers.SGD(
-                learning_rate=nn_cfg.INITIAL_LEARNING_RATE,
-                momentum=nn_cfg.MOMENTUM
-            ),
-            loss={
-                "value_head": "mean_squared_error",
-                "policy_head": "categorical_crossentropy"
-            },
-            loss_weights={
-                "policy_head": 1,
-                "value_head": 1
-            }
-        )
-
-        #self.learning_rate_scheduler = callbacks.LearningRateScheduler(self._adapt_learning_rate)
+            self._residual_block_init()
+        self._policy_head_init()
+        self._value_head_init()
 
 
-    def _conv_block(self, layers):
-        layers = Conv2D(
-            filters=nn_cfg.NUM_FILTERS,
-            kernel_size=nn_cfg.KERNEL_SIZE,
-            strides=nn_cfg.STRIDE,
-            data_format="channels_first",
-            padding="same",
-            kernel_regularizer=regularizers.l2(nn_cfg.L2_REG_CONST)
-        )(layers)
+    def call(self, x, training=False):
+        for layer in self.layer_list:
+                x = layer(x)
 
-        layers = BatchNormalization(axis=1)(layers)
-        layers = ReLU()(layers)
+        policy_out = x
+        value_out = x
 
-        return layers
+        for layer in self.policy_head_layers:
+            policy_out = layer(policy_out)
 
+        for layer in self.value_head_layers:
+            value_out = layer(value_out)
 
-    def _residual_block(self, inputs):
-        layers = Conv2D(
-            filters=nn_cfg.NUM_FILTERS,
-            kernel_size=nn_cfg.KERNEL_SIZE,
-            strides=nn_cfg.STRIDE,
-            data_format="channels_first",
-            padding="same",
-            kernel_regularizer=regularizers.l2(nn_cfg.L2_REG_CONST)
-        )(inputs)
-
-        layers = BatchNormalization(axis=1)(layers)
-        layers = ReLU()(layers)
-
-        layers = Conv2D(
-            filters=nn_cfg.NUM_FILTERS,
-            kernel_size=nn_cfg.KERNEL_SIZE,
-            strides=nn_cfg.STRIDE,
-            data_format="channels_first",
-            padding="same",
-            kernel_regularizer=regularizers.l2(nn_cfg.L2_REG_CONST)
-        )(layers)
-
-        layers = BatchNormalization(axis=1)(layers)
-        layers = Add()([inputs, layers])
-        layers = ReLU()(layers)
-
-        return layers
-
-
-    def _policy_head(self, layers):
-        layers = Conv2D(
-            filters=nn_cfg.NUM_FILTERS_POLICY,
-            kernel_size=nn_cfg.KERNEL_SIZE_POLICY,
-            strides=nn_cfg.STRIDE,
-            data_format="channels_first",
-            padding="same",
-            kernel_regularizer=regularizers.l2(nn_cfg.L2_REG_CONST)
-        )(layers)
-
-        layers = BatchNormalization(axis=1)(layers)
-        layers = ReLU()(layers)
-        layers = Flatten()(layers)
-        layers = Dense(
-            nn_cfg.NUM_POLICY_DENSE_LAYER_OUTPUT,
-            kernel_regularizer=regularizers.l2(nn_cfg.L2_REG_CONST),
-            name="policy_head"
-        )(layers)
-
-        return layers
-
-
-    def _value_head(self, layers):
-        layers = Conv2D(
-            filters=nn_cfg.NUM_FILTERS_HEAD,
-            kernel_size=nn_cfg.KERNEL_SIZE_HEAD,
-            strides=nn_cfg.STRIDE,
-            data_format="channels_first",
-            padding="same",
-        )(layers)
-
-        layers = BatchNormalization(axis=1)(layers)
-        layers = ReLU()(layers)
-        layers = Flatten()(layers)
-
-        layers = Dense(
-            nn_cfg.NUM_HEAD_DENSE_LAYER_OUTPUT_1,
-            kernel_regularizer=regularizers.l2(nn_cfg.L2_REG_CONST)
-        )(layers)
-
-        layers = ReLU()(layers)
-
-        layers = Dense(
-            nn_cfg.NUM_HEAD_DENSE_LAYER_OUTPUT_2,
-            activation="tanh",
-            kernel_regularizer=regularizers.l2(nn_cfg.L2_REG_CONST),
-            name="value_head"
-        )(layers)
-
-        return layers
-
-
+        return policy_out, value_out
         
+        
+    def predict_board(self, board):
+        return super(NeuralNetwork, self).predict(np.array([board.get_input_features()], dtype="float32"))
+
+
+    def _conv_block_init(self):
+        self.layer_list.append(
+            Conv2D(
+                filters=nn_cfg.NUM_FILTERS,
+                kernel_size=nn_cfg.KERNEL_SIZE,
+                strides=nn_cfg.STRIDE,
+                data_format="channels_first",
+                padding="same",
+                kernel_regularizer=regularizers.l2(nn_cfg.L2_REG_CONST)
+            )
+        )
+
+        self.layer_list.append(BatchNormalization(axis=1))
+        self.layer_list.append(ReLU())
+
+
+    def _residual_block_init(self):
+        inputs = self.layer_list[-1]
+    
+        self.layer_list.append(
+            Conv2D(
+                filters=nn_cfg.NUM_FILTERS,
+                kernel_size=nn_cfg.KERNEL_SIZE,
+                strides=nn_cfg.STRIDE,
+                data_format="channels_first",
+                padding="same",
+                kernel_regularizer=regularizers.l2(nn_cfg.L2_REG_CONST)
+            )
+        )
+
+        self.layer_list.append(BatchNormalization(axis=1))
+        self.layer_list.append(ReLU())
+
+        self.layer_list.append(
+            Conv2D(
+                filters=nn_cfg.NUM_FILTERS,
+                kernel_size=nn_cfg.KERNEL_SIZE,
+                strides=nn_cfg.STRIDE,
+                data_format="channels_first",
+                padding="same",
+                kernel_regularizer=regularizers.l2(nn_cfg.L2_REG_CONST)
+            )
+        )
+
+        self.layer_list.append(BatchNormalization(axis=1))
+        self.layer_list.append(add([self.layer_list[-1], inputs]))
+        self.layer_list.append(ReLU())
+
+
+    def _policy_head_init(self):
+        self.policy_head_layers.append(
+            Conv2D(
+                filters=nn_cfg.NUM_FILTERS_POLICY,
+                kernel_size=nn_cfg.KERNEL_SIZE_POLICY,
+                strides=nn_cfg.STRIDE,
+                data_format="channels_first",
+                padding="same",
+                kernel_regularizer=regularizers.l2(nn_cfg.L2_REG_CONST)
+            )
+        )
+
+        self.policy_head_layers.append(BatchNormalization(axis=1))
+        self.policy_head_layers.append(ReLU())
+        self.policy_head_layers.append(Flatten())
+        self.policy_head_layers.append(
+            Dense(
+                nn_cfg.NUM_POLICY_DENSE_LAYER_OUTPUT,
+                kernel_regularizer=regularizers.l2(nn_cfg.L2_REG_CONST),
+                name="policy_head"
+            )
+        )
+
+
+    def _value_head_init(self):
+        self.value_head_layers.append(
+            Conv2D(
+                filters=nn_cfg.NUM_FILTERS_HEAD,
+                kernel_size=nn_cfg.KERNEL_SIZE_HEAD,
+                strides=nn_cfg.STRIDE,
+                data_format="channels_first",
+                padding="same",
+            )
+        )
+
+        self.value_head_layers.append(BatchNormalization(axis=1))
+        self.value_head_layers.append(ReLU())
+        self.value_head_layers.append(Flatten())
+
+        self.value_head_layers.append(
+            Dense(
+                nn_cfg.NUM_HEAD_DENSE_LAYER_OUTPUT_1,
+                kernel_regularizer=regularizers.l2(nn_cfg.L2_REG_CONST)
+            )
+        )
+
+        self.value_head_layers.append(ReLU())
+
+        self.value_head_layers.append(
+            Dense(
+                nn_cfg.NUM_HEAD_DENSE_LAYER_OUTPUT_2,
+                activation="tanh",
+                kernel_regularizer=regularizers.l2(nn_cfg.L2_REG_CONST),
+                name="value_head"
+            )
+        )
+
+
+
 if __name__ == "__main__":
     nn = NeuralNetwork()
+    nn.compile(
+        loss={
+            "policy_head": "categorical_crossentropy",
+            "value_head": "mean_squared_error"
+        },
+        optimizer=optimizers.SGD(),
+        loss_weights={
+            "policy_head": 1,
+            "value_head": 1
+        }	
+	)
+    board = Board()
+    nn.predict_board(board)
